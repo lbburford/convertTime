116b8cbfab02f607b8d6129abded9bffec47162a
Levi Burford <lbburford@plymouth.edu>
2023-11-15 12:36:21 -0500

    First Commit-Commiting files to repo

---
+++ convert-time.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <assert.h>
+#include <fstream>
+
+std::fstream infile; // The stream that we use to open the file
+
+int main(int argc, char *argv[]) {
+    std::string str_line;
+    std::string time_string;
+
+    long int time_seconds;
+
+    // open input csv
+    infile.open(argv[1]); // Opens file stream from existing file
+
+    //open output csv
+
+    //get line
+    std::getline(infile, str_line);
+    // parse time
+
+    // convert time
+    time_seconds = std::stoi(str_line); //Convert the string to an integer (even though it is a double)
+
+    std::time_t time_object(time_seconds); //Convert to object of type time_t for localtime()
+
+    std::tm *tm_ptr = std::localtime(&time_object); //pointer to tm object
+    std::string ascii_time = std::asctime(tm_ptr); // Converts to human-readable date
+    
+    // write to file with new converted time
+    std::cout << "Timestamp: " << ascii_time << "\n";
+
+    return (0);
+}
---
+++ fileLine.cpp
@@ -0,0 +1,73 @@
+/* Practice working with classes.
+This is an attempt to answer exercise 14-4 in Practical C++ Programming 
+
+It gives a shell of a class and asks for the definition of member functions
+for handling lines in a file. 
+
+I chose to create an instance of the class though I suppose one could do it with static functions too.
+
+I am not sure that I did this the way it was intended but it seems to work anyway and was somewhat simple*/
+
+#include <iostream>
+#include <assert.h>
+#include <fstream>
+
+std::fstream infile; //The stream that we use to open the file
+
+/* Class definition:
+I added the private members line_position and char_position as well as a constructor that takes a string 
+that is the file name. */
+class line_number { 
+    private:
+        int line_position; //the current line
+        int char_position; //the number of chars up to the beginning of the line
+    public:
+        line_number(std::string file_name); //Constructor takes a filename of a text file with multiple lines
+        void goto_line(int line); //sets the position of the line in the input file
+        int get_current_line(); //returns the current line number of the input file
+        long int get_char_pos(); //return character position of line (number of chars before the beginning of the line)
+};
+
+line_number::line_number(std::string file_name) {
+    infile.open(file_name); //Opens file stream from existing file
+    char_position = 0; //Init char position
+    line_position = 0; //Init line position
+}
+
+void line_number::goto_line(int line) {
+    std::string str_line;
+
+    assert(line >= 0); //Make sure that we weren't passed a bad number
+
+    /* Loop until the end of the file or the line position is found.
+    If the end of the file is reached the line_position remains the last line. */
+    while(std::getline(infile, str_line)) { 
+        if(line == line_position) //Ends loop if line position has been reached
+            return;
+
+        //std::cout << "Line length: " << str_line.length() << "\n";
+
+        /* If line position has not been reached the characters are added to the counter 
+        before going on to the next line */
+        char_position += str_line.length(); 
+        ++line_position; //Before going to the next line of the file, iterate the line position holder
+    }
+    std::cout << "The end of the file was reached and the line_position was set at " << line_position << "\n";
+}
+
+int line_number::get_current_line() {
+    return (line_position); //Private member
+}
+
+long int line_number::get_char_pos() {
+    return (char_position); //Private member
+}
+
+int main() {
+    line_number ln("out.txt"); //Instantiate an object called ln
+
+    ln.goto_line(55); //Set line
+
+    std::cout << "Current line position: " << ln.get_current_line() << "\n"; //Output current line
+    std::cout << "Character position of line: " << ln.get_char_pos() << "\n"; //Output the number of characters before beginning of line
+}
